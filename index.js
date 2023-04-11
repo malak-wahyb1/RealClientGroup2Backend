44d88659b9a27eeb8c6d61a42ad3f16d64d259a5
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-04-11 12:34:55 +0300

    CrudForCustomer

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import PaymentRoute from "./routes/paymentRoute.js";
 import CategoryRoute from "./routes/categoryRoute.js";
 import newsLetterRoute from "./routes/newsLetterRoute.js";
 
-import ContactRoute from './routes/contactRoute.js'
-import SubCategoryRoute from './routes/subCategoryRoute.js'
+import ContactRoute from "./routes/contactRoute.js";
+import SubCategoryRoute from "./routes/subCategoryRoute.js";
 
 import OfferRoute from "./routes/offerRoute.js";
 
@@ -23,11 +23,13 @@ app.get("/", (req, res) => {
   res.send("you are connected!");
 });
 app.use("/payment", PaymentRoute);
-app.use('/category',CategoryRoute)
+app.use("/category", CategoryRoute);
 app.use("/newsletter", newsLetterRoute);
 app.use("/contact", ContactRoute);
-app.use("/subCategory",SubCategoryRoute)
-app.use("/offer",OfferRoute);
+app.use("/subCategory", SubCategoryRoute);
+app.use("/offer", OfferRoute);
+app.use("/customer", CutomerRoute);
+
 app.use("*", (req, res) => {
   res.status(404).send({ message: "404 Not Found" });
 });
---
+++ models/customerModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const customerSchema = new Schema(
+  {
+    name: {
+      type: "string",
+      required: true,
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+subCategorySchema.pre(["find", "findOne"], function () {
+  this.populate("category");
+});
+const Customer = model("customer", customerSchema);
+export default Customer;
